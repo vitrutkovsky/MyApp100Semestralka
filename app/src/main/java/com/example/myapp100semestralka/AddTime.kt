5c0c8ea1745c926aef00423b4156c916a899523c
vitrutkovsky <vit.rutkovsky@tul.cz>
2024-02-07 01:03:22 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapp100semestralka/AddTime.kt
@@ -0,0 +1,11 @@
+package com.example.myapp100semestralka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_time)
+    }
+}
